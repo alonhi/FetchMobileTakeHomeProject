b1e10f57e274e494db44869ec4311ca71b1376a3
idttm <andrew79911997@gmial.com>
2024-12-19 14:49:54 -0600

    Initial Commit

---
+++ FetchMobileTakeHomeProject/FetchMobileTakeHomeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchMobileTakeHomeProjectApp.swift
+//  FetchMobileTakeHomeProject
+//
+//  Created by Andrew Cheberyako on 19.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct FetchMobileTakeHomeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
